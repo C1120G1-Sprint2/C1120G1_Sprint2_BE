8dace3e5eb157ba7b6b03c9fee0a3a0473ed1e9f
Tuong Vi Nguyen <nttuongvi.36@gmail.com>
2021-06-20 22:53:42 +0700

    ViNTT update BE

--- src/main/java/com/c1120g1/cinema/controller/MovieTicketController.java
+++ src/main/java/com/c1120g1/cinema/controller/MovieTicketController.java
@@ -1,9 +1,20 @@
 package com.c1120g1.cinema.controller;
 
+import com.c1120g1.cinema.entity.MovieTicket;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class MovieTicketController {
+
+    @PostMapping("/api/movie-ticket/create")
+    public ResponseEntity<Void> create(@RequestBody MovieTicket movieTicket) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
